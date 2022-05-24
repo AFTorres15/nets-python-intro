2cce11d1904189192c0a7f32b9b8d9ba22ec7939
AFTorres <torreand15@gmail.com>
2022-05-24 00:38:42 -0500

    wordCOunt.py can now get the words cound how often they occur and sort them in decending order

--- wordCount.py
+++ wordCount.py
@@ -1,12 +1,13 @@
 import sys  # command line arguments
 import re  # regular expression tools
 import os  # checking if file exists
-import subprocess  # executing program
+# import subprocess  # executing program
 
 # set input and output files
 if len(sys.argv) != 3:
     print("Correct usage: wordCount.py <input text file> <output file> ")
     exit()
+
 
 inputFname = sys.argv[1]
 outputFname = sys.argv[2]
@@ -23,4 +24,32 @@ if not os.path.exists(outputFname):
     # print("wordCount output file %s doesn't exist! Exiting" % outputFname)
 else:
     outputFile = open(outputFname, 'w+')
-    exit()
+
+# word count
+words = 0
+
+# word list
+master = {}
+
+
+
+
+# open input file
+# remove punctuation and new lines
+with open(inputFname, 'r') as inputFile:
+    for line in inputFile:
+        # get rid of newline characters
+        line = line.strip()
+        # split line on whitespace and punctuation
+        line = re.sub(r'[^\w\s]', '', line)
+        word = re.split('[ \t]', line)
+        for element in word:
+            if master.get(element) is None:
+                master[element] = 1
+            else:
+                master[element] = master[element]+1
+sortedMaster = sorted(master, reverse=True)
+for item in sortedMaster:
+    print(item , master.get(item))
+
+exit()
